db86c223830072bff82235749b237c167bfc1fe0
Leigh Dotey <leighdotey@gmail.com>
2024-12-24 12:14:18 -0500

    add select hiker drop down on add trail form

--- client/src/components/NewTrailForm.js
+++ client/src/components/NewTrailForm.js
@@ -5,12 +5,12 @@ import { MyContext } from "./AppContext";
 import { useParams } from "react-router-dom";
 
 function CreateTrail() {
-  const { setTrails } = useContext(MyContext);
+  const { setTrails, hikers } = useContext(MyContext);
   const { id } = useParams();
   console.log(useParams());
 
   const parkId = parseInt(id, 10);
-  const hikerId = 99;
+  //   const hikerId = 4;
 
   const formSchema = yup.object().shape({
     name: yup.string().required("Trail name is required"),
@@ -26,7 +26,7 @@ function CreateTrail() {
       difficulty: "",
       dog_friendly: false,
       park_id: parkId,
-      hiker_id: hikerId,
+      hiker_id: "",
     },
 
     validationSchema: formSchema,
@@ -92,9 +92,25 @@ function CreateTrail() {
           checked={formik.values.dog_friendly}
         />
         <p style={{ color: "red" }}>{formik.errors.dog_friendly}</p>
+        <label htmlFor="hiker_id">Select Hiker:</label>
+        <br />
+        <select
+          id="hiker_id"
+          name="hiker_id"
+          onChange={formik.handleChange}
+          value={formik.values.hiker_id}
+        >
+          <option value="">Select a hiker</option>
+          {hikers.map((hiker) => (
+            <option key={hiker.id} value={hiker.id}>
+              {hiker.name}
+            </option>
+          ))}
+        </select>
+        <p style={{ color: "red" }}>{formik.errors.hiker_id}</p>
 
         <input type="hidden" name="park_id" value={formik.values.park_id} />
-        <input type="hidden" name="hiker_id" value={8} />
+        {/* <input type="hidden" name="hiker_id" value={8} /> */}
 
         <button type="submit">Create Trail</button>
       </form>
